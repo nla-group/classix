9c02b57836503855f8eeaac2bf3a99583eb2aa6b
chenxinye <cclcquant@yahoo.com>
2022-02-02 23:32:17 +0000

    modified runtest.py

--- runtest.py
+++ runtest.py
@@ -32,7 +32,7 @@ for tol in np.arange(0.5,1.1,0.1):
     clx.fit_transform(vdu_signals)
     
     # version 0.2.0
-    np.save('classix/data/checkpoint_' + str(tol) + '.npy', clx.labels_) 
+    # np.save('classix/data/checkpoint_' + str(tol) + '.npy', clx.labels_) 
     
     # test new version
     checkpoint = np.load('classix/data/checkpoint_' + str(tol) + '.npy')
