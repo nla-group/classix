10f4f3c0bc1617ba2fe039e37d7bcf835d9a163c
Null <42580530+chenxinye@users.noreply.github.com>
2022-02-03 15:44:47 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,4 +59,4 @@ html_theme = "sphinx_rtd_theme" # html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_style = 'css/custom.css'
+html_style = 'css/_.css'
