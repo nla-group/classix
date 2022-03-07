887683d7c80d2a29a210bd03c67804d14dcb2b8a
chenxinye <cclcquant@yahoo.com>
2022-03-07 08:04:50 +0000

    update version 0.5.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import logging
 import setuptools
 from distutils.errors import CCompilerError, DistutilsExecError, DistutilsPlatformError
 
-_version="0.5.2"
+_version="0.5.3"
 logging.basicConfig()
 log = logging.getLogger(__file__)
 
