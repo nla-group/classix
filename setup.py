bdc34e4f3a71c8c876821df84cee71d79b359f93
null <42580530+chenxinye@users.noreply.github.com>
2022-04-20 16:06:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import logging
 import setuptools
 from distutils.errors import CCompilerError, DistutilsExecError, DistutilsPlatformError
 
-_version="0.5.8"
+_version="0.5.9"
 logging.basicConfig()
 log = logging.getLogger(__file__)
 
