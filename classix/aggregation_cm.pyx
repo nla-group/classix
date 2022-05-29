2c43254abf59154fb55bd9632f55a4dd26ad6c61
null <42580530+chenxinye@users.noreply.github.com>
2022-05-29 14:00:15 +0100

    Update aggregation_cm.pyx

--- classix/aggregation_cm.pyx
+++ classix/aggregation_cm.pyx
@@ -29,11 +29,10 @@
 
 cimport cython
 import numpy as np
-cimport numpy as np
-from scipy.linalg import get_blas_funcs, eigh
+cimport numpy as np 
 # from cython.parallel import prange
 # from sklearn.decomposition import PCA
-# from scipy.sparse.linalg import svds
+from scipy.sparse.linalg import svds
 # from libc.string cimport strcmp
 np.import_array()
 
@@ -41,23 +40,18 @@ np.import_array()
 @cython.wraparound(False)
 @cython.binding(True)
 
-
 cpdef aggregate(double[:,:] data, str sorting, double tol=0.5):
     """Aggregate the data
-
     Parameters
     ----------
     data : numpy.ndarray
         The input that is array-like of shape (n_samples,).
-
     sorting : str
         The sorting way refered for aggregation, default='pca', other options: 'norm-mean', 'norm-orthant'.
-
     tol : float
         The tolerance to control the aggregation, if the distance between the starting point 
         and the object is less than or equal than the tolerance,
         the object should allocated to the group which starting point belongs to.  
-
     Returns
     -------
     labels (numpy.ndarray) : 
@@ -69,12 +63,11 @@ cpdef aggregate(double[:,:] data, str sorting, double tol=0.5):
     nr_dist (int) :
         The number of pairwise distance calculations.
     """
-
     cdef Py_ssize_t fdim = data.shape[1] # feature dimension
     cdef Py_ssize_t len_ind = data.shape[0] # size of data
-    cdef double[:] sort_vals, _
+    cdef double[:] sort_vals
     cdef double[:, :] c_data = np.empty((len_ind, fdim), dtype=np.float64)
-    cdef double[:, :] U1 # , _  # = np.empty((len_ind, ), dtype=float)
+    cdef double[:, :] U1, _  # = np.empty((len_ind, ), dtype=float)
     cdef long long[:] ind # = np.empty((len_ind, ), dtype=int)
     cdef Py_ssize_t sp # starting point index
     cdef unsigned int lab=0, nr_dist=0, num_group
@@ -88,7 +81,6 @@ cpdef aggregate(double[:,:] data, str sorting, double tol=0.5):
         c_data[...] = data
         sort_vals = np.linalg.norm(c_data, ord=2, axis=1)
         ind = np.argsort(sort_vals)
-
     elif sorting == "pca":
         # pca = PCA(n_components=1) 
         # sort_vals = pca.fit_transform(data_memview).reshape(-1)
@@ -98,21 +90,16 @@ cpdef aggregate(double[:,:] data, str sorting, double tol=0.5):
         # data = data - data.mean(axis=0) -- already done in the clustering.fit_transform
         c_data = data - np.mean(data, axis=0)
         if data.shape[1]>1:
-            gemm = get_blas_funcs("gemm", [c_data.T, c_data])
-            _, U1 = eigh(gemm(1, c_data.T, c_data), subset_by_index=[fdim-1, fdim-1])
-            sort_vals = c_data@U1.reshape(-1)
-            # U1, s1, _ = svds(c_data, k=1, return_singular_vectors="u")
-            # sort_vals = U1[:,0]*s1[0]
+            U1, s1, _ = svds(c_data, k=1, return_singular_vectors="u")
+            sort_vals = U1[:,0]*s1[0]
         else:
             sort_vals = c_data[:,0]
         sort_vals = sort_vals*np.sign(-sort_vals[0]) # flip to enforce deterministic output
         ind = np.argsort(sort_vals)
-
     else: # no sorting
         sort_vals = np.zeros(len_ind)
         ind = np.arange(len_ind)
-
-
+    
     for i in range(len_ind): 
         sp = ind[i] # starting point
         
@@ -135,18 +122,17 @@ cpdef aggregate(double[:,:] data, str sorting, double tol=0.5):
             dist = 0
             for coord in range(fdim):
                 dist += (clustc[coord] - c_data[j,coord])**2
-
             nr_dist += 1
-
             if dist <= tol**2:
                 num_group += 1
                 labels[j] = lab
-                
+
         splist.append([sp, sort_vals[sp], num_group])  # list of [ starting point index of current group, sorting key, and number of group elements ]
-        
+
         lab += 1
-        
+  
     return np.asarray(labels), splist, nr_dist
+
 
 
 # move to lite_func.py
@@ -186,8 +172,5 @@ cpdef aggregate(double[:,:] data, str sorting, double tol=0.5):
 #         labels.append(list(set(sub)))
 #         
 #     return labels
-
-
-
 # cdef check_if_intersect(list g1, list g2):
 #     return set(g1).intersection(g2) != set()
