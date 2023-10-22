a0ca042b0f7cf3f39510349a838cdbaada770842
the null <42580530+chenxinye@users.noreply.github.com>
2023-10-22 22:34:12 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,13 +48,15 @@ pygments_style = 'lovelace'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme" # html_theme = 'alabaster'
-html_theme_options = {
-    'logo_only': True,
-    'navigation_depth': 5,
-}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_style = 'css/_.css'
+html_theme = "sphinx_rtd_theme" # html_theme = 'alabaster'
+html_logo = "classix_logo.png"
+html_theme_options = {
+    'logo_only': True,
+    'navigation_depth': 5,
+}
