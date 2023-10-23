4fab3530db8cdfad03175930dd5e7b8f8b28aa3e
the null <42580530+chenxinye@users.noreply.github.com>
2023-10-23 20:18:34 +0200

    Update aggregation_c.pyx

--- classix/aggregation_c.pyx
+++ classix/aggregation_c.pyx
@@ -43,6 +43,99 @@ np.import_array()
 @cython.wraparound(False)
 @cython.binding(True)
 
+cpdef precompute_aggregate_pca(np.ndarray[np.float64_t, ndim=2] data, double tol=0.5):
+    """Aggregate the data with PCA using precomputation
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        The input that is array-like of shape (n_samples,).
+
+    sorting : str
+        The sorting method for aggregation, default='pca', other options: 'norm-mean', 'norm-orthant'.
+
+    tol : float
+        The tolerance to control the aggregation. if the distance between the starting point 
+        of a group and another data point is less than or equal to the tolerance,
+        the point is allocated to that group.  
+
+    Returns
+    -------
+    labels (numpy.ndarray) : 
+        The group categories of the data after aggregation.
+    
+    splist (list) : 
+        The list of the starting points.
+    
+    nr_dist (int) :
+        The number of pairwise distance calculations.
+    """
+    
+    cdef int num_group
+    cdef Py_ssize_t fdim = data.shape[1] # feature dimension
+    cdef Py_ssize_t len_ind = data.shape[0] # size of data
+    cdef np.ndarray[np.float64_t, ndim=2] U1
+
+    cdef int nr_dist = 0 
+    cdef int lab = 0 
+    cdef np.ndarray[np.int64_t, ndim=1] labels = np.full(len_ind, -1, dtype=int)
+    cdef list splist = list() 
+    cdef np.ndarray[np.float64_t, ndim=1] sort_vals
+    cdef np.ndarray[np.float64_t, ndim=1] clustc
+    cdef np.ndarray[np.int64_t, ndim=1] ind
+    cdef Py_ssize_t i, j
+    
+    cdef double half_r2 = tol**2 * 0.5
+    cdef np.ndarray[np.float64_t, ndim=1] half_nrm2 = np.einsum('ij,ij->i', data, data) * 0.5
+    
+    cdef double rhs
+
+    if fdim > 1:
+        if fdim <= 3: # memory inefficient
+            gemm = get_blas_funcs("gemm", [data.T, data])
+            _, U1 = eigh(gemm(1, data.T, data), subset_by_index=[fdim-1, fdim-1])
+            sort_vals = data@U1.reshape(-1)
+        else:
+            U1, s1, _ = svds(data, k=1, return_singular_vectors=True)
+            sort_vals = U1[:,0]*s1[0]
+
+    else:
+        sort_vals = data[:,0]
+        
+    sort_vals = sort_vals*np.sign(-sort_vals[0]) # flip to enforce deterministic output
+
+    ind = np.argsort(sort_vals)
+
+    for i in range(len_ind): 
+        if labels[i] >= 0:
+            continue
+        
+        clustc = data[i,:] 
+        labels[i] = lab
+        num_group = 1
+        splist.append((i, sort_vals[i], num_group))
+            
+        rhs = half_r2 - half_nrm2[i] # right-hand side of norm ineq.
+        last_j = np.searchsorted(sort_vals, tol + sort_vals[i], side='right')
+        ips = np.matmul(data[i+1:last_j,:], clustc)
+        nr_dist += last_j - i - 1
+
+        for j in range(i+1, last_j):
+                    
+            if labels[j] >= 0:
+                continue
+                
+            if half_nrm2[j] - ips[j-i-1] <= rhs:
+                num_group += 1
+                labels[j] = lab
+
+        lab += 1
+
+    labels = labels[np.argsort(ind)]
+    return labels, splist, nr_dist, ind
+
+
+
 cpdef precompute_aggregate(np.ndarray[np.float64_t, ndim=2] data, str sorting="pca", double tol=0.5):
     """Aggregate the data using precomputation
 
@@ -95,10 +188,10 @@ cpdef precompute_aggregate(np.ndarray[np.float64_t, ndim=2] data, str sorting="p
 
     if sorting == "norm-mean" or sorting == "norm-orthant": 
         sort_vals = np.linalg.norm(data, ord=2, axis=1)
-        ind = np.argsort(sort_vals)
+        
 
     elif sorting == "pca":
-        if data.shape[1]>1:
+        if fdim > 1:
             if fdim <= 3: # memory inefficient
                 gemm = get_blas_funcs("gemm", [data.T, data])
                 _, U1 = eigh(gemm(1, data.T, data), subset_by_index=[fdim-1, fdim-1])
@@ -111,12 +204,13 @@ cpdef precompute_aggregate(np.ndarray[np.float64_t, ndim=2] data, str sorting="p
             sort_vals = data[:,0]
             
         sort_vals = sort_vals*np.sign(-sort_vals[0]) # flip to enforce deterministic output
-        ind = np.argsort(sort_vals)
+        
 
     else: # no sorting
         sort_vals = np.zeros(len_ind)
-        ind = np.arange(len_ind)
     
+    ind = np.argsort(sort_vals)
+
     for i in range(len_ind):
         
         sp = ind[i] 
