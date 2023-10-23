16a339ff9c7948f14cf831f5df3ac35cd03bf1b6
the null <42580530+chenxinye@users.noreply.github.com>
2023-10-23 18:07:45 +0200

    Update aggregation_cm.pyx

--- classix/aggregation_cm.pyx
+++ classix/aggregation_cm.pyx
@@ -38,16 +38,13 @@ np.import_array()
 @cython.wraparound(False)
 @cython.binding(True)
 
-cpdef precompute_aggregate(double[:,:] data, str sorting, double tol=0.5):
-    """Aggregate the data using precomputation
+cpdef precompute_aggregate_pca(double[:,:] data, double tol=0.5):
+    """Aggregate the data with PCA using precomputation
 
     Parameters
     ----------
     data : numpy.ndarray
         The input that is array-like of shape (n_samples,).
-    
-    sorting : str
-        The sorting way refered for aggregation, default='pca', other options: 'norm-mean', 'norm-orthant'.
     
     tol : float
         The tolerance to control the aggregation, if the distance between the starting point 
@@ -85,28 +82,19 @@ cpdef precompute_aggregate(double[:,:] data, str sorting, double tol=0.5):
     cdef double[:] half_nrm2 = np.einsum('ij,ij->i', data, data) * 0.5
     cdef double rhs
 
-
-
-    if sorting == "norm-mean" or sorting == "norm-orthant":
-        sort_vals = np.linalg.norm(data, ord=2, axis=1)
-        
-    elif sorting == "pca":
-        if fdim > 1:
-            if fdim <= 3: # memory inefficient
-                    gemm = get_blas_funcs("gemm", [data.T, data])
-                    _, U1 = eigh(gemm(1, data.T, data), subset_by_index=[fdim-1, fdim-1])
-                    sort_vals = data@U1.reshape(-1)
-            else:
-                U1, s1, _ = svds(np.asarray(data), k=1, return_singular_vectors=True)
-                sort_vals = U1[:,0]*s1[0]
+    if fdim > 1:
+        if fdim <= 3: # memory inefficient
+                gemm = get_blas_funcs("gemm", [data.T, data])
+                _, U1 = eigh(gemm(1, data.T, data), subset_by_index=[fdim-1, fdim-1])
+                sort_vals = data@U1.reshape(-1)
         else:
-            sort_vals = data[:,0]
-            
-        sort_vals = sort_vals*np.sign(-sort_vals[0]) # flip to enforce deterministic output
+            U1, s1, _ = svds(np.asarray(data), k=1, return_singular_vectors=True)
+            sort_vals = U1[:,0]*s1[0]
+    else:
+        sort_vals = data[:,0]
         
-    else: # no sorting
-        sort_vals = np.zeros(len_ind)
-
+    sort_vals = sort_vals*np.sign(-sort_vals[0]) # flip to enforce deterministic output
+        
     ind = np.argsort(sort_vals)
     data = data.base[ind]
 
@@ -136,6 +124,119 @@ cpdef precompute_aggregate(double[:,:] data, str sorting, double tol=0.5):
         lab += 1
 
     labels = labels.base[np.argsort(ind)]
+    return np.asarray(labels), splist, nr_dist, ind
+
+
+
+cpdef precompute_aggregate(double[:,:] data, str sorting, double tol=0.5):
+    """Aggregate the data using precomputation
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        The input that is array-like of shape (n_samples,).
+    
+    sorting : str
+        The sorting way refered for aggregation, default='pca', other options: 'norm-mean', 'norm-orthant'.
+    
+    tol : float
+        The tolerance to control the aggregation, if the distance between the starting point 
+        and the object is less than or equal than the tolerance,
+        the object should allocated to the group which starting point belongs to.  
+    
+    
+    Returns
+    -------
+    labels (numpy.ndarray) : 
+        The group category of the data after aggregation.
+    
+    splist (list) : 
+        The list of the starting points.
+    
+    nr_dist (int) :
+        The number of pairwise distance calculations.
+    """
+
+    cdef Py_ssize_t fdim = data.shape[1] # feature dimension
+    cdef Py_ssize_t len_ind = data.shape[0] # size of data
+    cdef double[:] sort_vals
+    cdef double[:, :] U1, _  # = np.empty((len_ind, ), dtype=float)
+    cdef long long[:] ind # = np.empty((len_ind, ), dtype=int)
+    cdef Py_ssize_t sp # starting point index
+    cdef unsigned int lab=0, nr_dist=0, num_group
+    cdef double[:] clustc # starting point coordinates
+    cdef double dist
+    cdef long[:] labels = np.full(len_ind, -1, dtype=int) # np.zeros(, dtype=int) - 1
+    cdef list splist = list() # list of starting points
+    cdef Py_ssize_t i, ii, j, coord
+    
+    cdef double half_r2 = tol**2 * 0.5
+    cdef double[:] half_nrm2 = np.einsum('ij,ij->i', data, data) * 0.5
+    cdef double[:] dataj
+    cdef double rhs
+
+
+    if sorting == "norm-mean" or sorting == "norm-orthant":
+        sort_vals = np.linalg.norm(data, ord=2, axis=1)
+        ind = np.argsort(sort_vals)
+    
+    elif sorting == "pca":
+        if fdim > 1:
+            if fdim <= 3: # memory inefficient
+                    gemm = get_blas_funcs("gemm", [data.T, data])
+                    _, U1 = eigh(gemm(1, data.T, data), subset_by_index=[fdim-1, fdim-1])
+                    sort_vals = data@U1.reshape(-1)
+            else:
+                U1, s1, _ = svds(np.asarray(data), k=1, return_singular_vectors=True)
+                sort_vals = U1[:,0]*s1[0]
+        else:
+            sort_vals = data[:,0]
+            
+        sort_vals = sort_vals*np.sign(-sort_vals[0]) # flip to enforce deterministic output
+        ind = np.argsort(sort_vals)
+    
+    else: # no sorting
+        sort_vals = np.zeros(len_ind)
+        ind = np.arange(len_ind)
+
+    for i in range(len_ind): 
+        sp = ind[i] # starting point
+        
+        if labels[sp] >= 0:
+            continue
+        
+        clustc = data[sp,:] 
+        labels[sp] = lab
+        num_group = 1
+            
+        rhs = half_r2 - half_nrm2[sp] # right-hand side of norm ineq.
+
+        for ii in range(i+1, len_ind): 
+            j = ind[ii]
+                    
+            if labels[j] >= 0:
+                continue
+                
+            if sort_vals[j] - sort_vals[sp] > tol:
+                break       
+            
+            dist = 0
+
+            dataj = data[j]
+            for coord in range(fdim):
+                dist += clustc[coord] * dataj[coord]
+            
+            nr_dist += 1
+            
+            if half_nrm2[j] - dist <= rhs:
+                num_group += 1
+                labels[j] = lab
+
+        splist.append((sp, sort_vals[sp], num_group))  
+        # list of [ starting point index of current group, sorting key, and number of group elements ]
+
+        lab += 1
+  
     return np.asarray(labels), splist, nr_dist, ind
 
 
