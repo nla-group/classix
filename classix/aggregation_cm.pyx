5de8b82b5dfbd37eb2e54b1cb55ff3791dcb1ae6
the null <42580530+chenxinye@users.noreply.github.com>
2023-10-23 17:46:23 +0200

    Update aggregation_cm.pyx

--- classix/aggregation_cm.pyx
+++ classix/aggregation_cm.pyx
@@ -31,6 +31,7 @@ cimport cython
 import numpy as np
 cimport numpy as np 
 from scipy.sparse.linalg import svds
+from scipy.linalg import get_blas_funcs, eigh
 np.import_array()
 
 @cython.boundscheck(False)
@@ -66,81 +67,75 @@ cpdef precompute_aggregate(double[:,:] data, str sorting, double tol=0.5):
         The number of pairwise distance calculations.
     """
 
-    cdef Py_ssize_t fdim = data.shape[1] # feature dimension
-    cdef Py_ssize_t len_ind = data.shape[0] # size of data
+    cdef Py_ssize_t len_ind, fdim, last_j
+    len_ind, fdim = data.base.shape
+
     cdef double[:] sort_vals
-    cdef double[:, :] U1, _  # = np.empty((len_ind, ), dtype=float)
-    cdef long long[:] ind # = np.empty((len_ind, ), dtype=int)
-    cdef Py_ssize_t sp # starting point index
+    cdef double[:, :] U1, _
+    cdef long long[:] ind
     cdef unsigned int lab=0, nr_dist=0, num_group
-    cdef double[:] clustc # starting point coordinates
-    cdef double dist
-    cdef long[:] labels = np.full(len_ind, -1, dtype=int) # np.zeros(, dtype=int) - 1
-    cdef list splist = list() # list of starting points
-    cdef Py_ssize_t i, ii, j, coord
+    cdef double[:] clustc 
+    
+
+    cdef long[:] labels = np.full(len_ind, -1, dtype=int) 
+    cdef list splist = list() 
+    cdef Py_ssize_t i, j
     
     cdef double half_r2 = tol**2 * 0.5
     cdef double[:] half_nrm2 = np.einsum('ij,ij->i', data, data) * 0.5
-    cdef double[:] dataj
     cdef double rhs
+
 
 
     if sorting == "norm-mean" or sorting == "norm-orthant":
         sort_vals = np.linalg.norm(data, ord=2, axis=1)
-        ind = np.argsort(sort_vals)
-    
+        
     elif sorting == "pca":
-        if data.shape[1]>1:
-            U1, s1, _ = svds(np.asarray(data), k=1, return_singular_vectors=True)
-            sort_vals = U1[:,0]*s1[0]
+        if fdim > 1:
+            if fdim <= 3: # memory inefficient
+                    gemm = get_blas_funcs("gemm", [data.T, data])
+                    _, U1 = eigh(gemm(1, data.T, data), subset_by_index=[fdim-1, fdim-1])
+                    sort_vals = data@U1.reshape(-1)
+            else:
+                U1, s1, _ = svds(np.asarray(data), k=1, return_singular_vectors=True)
+                sort_vals = U1[:,0]*s1[0]
         else:
             sort_vals = data[:,0]
             
         sort_vals = sort_vals*np.sign(-sort_vals[0]) # flip to enforce deterministic output
-        ind = np.argsort(sort_vals)
-    
+        
     else: # no sorting
         sort_vals = np.zeros(len_ind)
-        ind = np.arange(len_ind)
+
+    ind = np.argsort(sort_vals)
+    data = data.base[ind]
 
     for i in range(len_ind): 
-        sp = ind[i] # starting point
-        
-        if labels[sp] >= 0:
+        if labels[i] >= 0:
             continue
         
-        clustc = data[sp,:] 
-        labels[sp] = lab
+        clustc = data[i,:] 
+        labels[i] = lab
         num_group = 1
+        splist.append((i, sort_vals[i], num_group))
             
-        rhs = half_r2 - half_nrm2[sp] # right-hand side of norm ineq.
+        rhs = half_r2 - half_nrm2[i] # right-hand side of norm ineq.
+        last_j = np.searchsorted(sort_vals, tol + sort_vals[i], side='right')
+        ips = np.matmul(data.base[i+1:last_j,:], clustc)
+        nr_dist += last_j - i - 1
 
-        for ii in range(i+1, len_ind): 
-            j = ind[ii]
+        for j in range(i+1, last_j):
                     
             if labels[j] >= 0:
                 continue
                 
-            if sort_vals[j] - sort_vals[sp] > tol:
-                break       
-            
-            dist = 0
-
-            dataj = data[j]
-            for coord in range(fdim):
-                dist += clustc[coord] * dataj[coord]
-            
-            nr_dist += 1
-            
-            if half_nrm2[j] - dist <= rhs:
+            if half_nrm2[j] - ips[j-i-1] <= rhs:
                 num_group += 1
                 labels[j] = lab
 
-        splist.append((sp, sort_vals[sp], num_group))  
-        # list of [ starting point index of current group, sorting key, and number of group elements ]
-
         lab += 1
-  
+
+    labels = labels.base[np.argsort(ind)]
     return np.asarray(labels), splist, nr_dist, ind
 
 
