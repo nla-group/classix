2751648698bd1b8d8a4e392d64f64c001ed33207
the null <42580530+chenxinye@users.noreply.github.com>
2023-11-02 12:55:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError as e:
     warnings.warn(e.args[0])
     from setuptools.command.build_ext import build_ext
     
-_version="0.8.9"
+_version="0.9.1"
 logging.basicConfig()
 log = logging.getLogger(__file__)
 
