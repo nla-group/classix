cfdea016779a2306b72611f24d7c87e178e4e1d5
the null <42580530+chenxinye@users.noreply.github.com>
2023-11-05 13:03:06 +0100

    Update unittests.py

--- unittests.py
+++ unittests.py
@@ -251,7 +251,7 @@ class TestClassix(unittest.TestCase):
             clx.explain(0, 2008,  plot=True, add_arrow=True, directed_arrow=-1, savefig=True)
             clx.explain(0, 2008,  plot=True, add_arrow=True, directed_arrow=1, savefig=True)
 
-            clx = CLASSIX(radius=0.5, group_merging='distance', minPts=4999)
+            clx = CLASSIX(radius=0.5, group_merging='distance', minPts=4999, mergeTinyGroups=False)
             clx.explain(0, 2008,  plot=True, add_arrow=True, directed_arrow=-1, savefig=True)
         except:
             checkpoint = 0
