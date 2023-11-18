fdb46964c76259f024befab1ae3c8f8f0b8f60f2
the null <42580530+chenxinye@users.noreply.github.com>
2023-11-18 05:51:19 +0100

    Update unittests.py

--- unittests.py
+++ unittests.py
@@ -347,7 +347,7 @@ class TestClassix(unittest.TestCase):
         checkpoint = 1
         try:
             for dn in ['vdu_signals', 'Iris', 'Dermatology', 'Ecoli', 'Glass', 'Banknote', 'Seeds', 
-                       'Phoneme', 'Wine', 'CovidENV', 'Covid3MC', 'NA']:
+                       'Phoneme', 'Wine', 'NA']:
                 loadData(name=dn)
         except:
             checkpoint = 0
