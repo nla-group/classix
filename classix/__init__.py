58040b7496ac9b3f888ef9f60c77727fbaadf3ed
the null <42580530+chenxinye@users.noreply.github.com>
2023-11-19 14:30:42 +0100

    Update __init__.py

--- classix/__init__.py
+++ classix/__init__.py
@@ -9,3 +9,6 @@ from .clustering import cython_is_available
 from .clustering import calculate_cluster_centers
 from .clustering import normalization
 
+
+"""For Matlab's users to check the CLASSIX's version."""
+version = __version__
