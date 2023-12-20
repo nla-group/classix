5f3d5c67763d5f836946607cc47c383de8acab95
chenxinye <cclcquant@yahoo.com>
2023-12-20 23:24:36 +0100

    add new files

--- build/lib.linux-x86_64-3.11/classix/__init__.py
+++ build/lib.linux-x86_64-3.11/classix/__init__.py
@@ -1,5 +1,5 @@
 
-__version__ = '1.2.2'                  
+__version__ = '1.2.3'                  
 __enable_cython__ = True 
 
 from .clustering import CLASSIX
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError as e:
     warnings.warn(e.args[0])
     from setuptools.command.build_ext import build_ext
     
-_version="1.2.2"
+_version="1.2.3"
 logging.basicConfig()
 log = logging.getLogger(__file__)
 
