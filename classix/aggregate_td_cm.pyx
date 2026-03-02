3697f524da113a1c0582589a9be1c5b60f21d820
the-null <cclcquant@yahoo.com>
2026-03-02 21:29:00 +0100

    Update aggregate_td_cm.pyx

--- classix/aggregate_td_cm.pyx
+++ classix/aggregate_td_cm.pyx
@@ -9,6 +9,7 @@ cimport numpy as cnp
 import scipy.sparse as sparse
 from libcpp.vector cimport vector
 
+
 cdef Py_ssize_t bisect_right(double[:] a, double x, Py_ssize_t lo, Py_ssize_t hi) nogil:
     cdef Py_ssize_t mid
     while lo < hi:
@@ -19,76 +20,79 @@ cdef Py_ssize_t bisect_right(double[:] a, double x, Py_ssize_t lo, Py_ssize_t hi
             lo = mid + 1
     return lo
 
+
 def aggregate_tanimoto(
-    double[:, :] data, 
+    double[:, :] data,
     double radius
 ):
+    """Tanimoto leader-follower clustering (sphere-exclusion).
+    Fully cross-platform (Windows + Linux + macOS)."""
+    if radius <= 0.0 or radius >= 1.0:
+        raise ValueError("radius must be in (0, 1)")
+
     cdef Py_ssize_t n = data.shape[0]
-    
+
     cdef double[:] sort_vals = np.sum(data, axis=1)
-    
-    cdef long[:] ind = np.argsort(sort_vals, kind='stable')
-    
+
+    cdef cnp.intp_t[:] ind = np.argsort(sort_vals, kind='stable')
+
     cdef double[:, :] data_sorted = np.asarray(data)[ind]
     cdef double[:] sort_vals_sorted = np.asarray(sort_vals)[ind]
-    
+
     datas = sparse.csr_matrix(np.asarray(data_sorted))
     cdef double[:] csr_data = datas.data
     cdef int[:] csr_indices = datas.indices
     cdef int[:] csr_indptr = datas.indptr
 
-    cdef long[:] labels = np.full(n, -1, dtype=np.int64)
-    cdef vector[long] splist
-    cdef vector[long] group_sizes
-    
-    splist.reserve(n // 10) 
+    cdef cnp.intp_t[:] labels = np.full(n, -1, dtype=np.intp)
+    cdef vector[cnp.intp_t] splist
+    cdef vector[cnp.intp_t] group_sizes
+
+    splist.reserve(n // 10)
     group_sizes.reserve(n // 10)
 
     cdef Py_ssize_t i, j, k, last_j
-    cdef long lab = 0
-    cdef long nr_dist = 0
-    cdef double sv_i, search_radius, dot, threshold_val
-    
-    cdef double rhs = 1.0 / (1.0 - radius) + 1.0
-    cdef double rhsi = 1.0 / rhs
-    
-    
+    cdef cnp.intp_t lab = 0
+    cdef Py_ssize_t nr_dist = 0
+    cdef double sv_i, search_upper, dot
+    cdef double multiplier = radius / (1.0 + radius)
+
     for i in range(n):
         if labels[i] >= 0:
             continue
-        
+
         labels[i] = lab
         splist.push_back(i)
         group_sizes.push_back(1)
-        
+
         sv_i = sort_vals_sorted[i]
-        search_radius = sv_i / (1.0 - radius)
-        
-        last_j = bisect_right(sort_vals_sorted, search_radius, i + 1, n)
-        
+        search_upper = sv_i / radius
+
+        last_j = bisect_right(sort_vals_sorted, search_upper, i + 1, n)
+
         if last_j > i + 1:
-            nr_dist += (last_j - (i + 1)) 
-            
+            nr_dist += (last_j - (i + 1))
+
             for j in range(i + 1, last_j):
                 if labels[j] != -1:
                     continue
-                
+
                 dot = 0.0
-                for k in range(csr_indptr[j], csr_indptr[j+1]):
-                    # data_sorted[i, col] * val
+                for k in range(csr_indptr[j], csr_indptr[j + 1]):
                     dot += data_sorted[i, csr_indices[k]] * csr_data[k]
-                
-                if (dot / (sv_i + sort_vals_sorted[j])) >= rhsi:
+
+                if dot >= multiplier * (sv_i + sort_vals_sorted[j]):
                     labels[j] = lab
                     group_sizes[lab] += 1
-        
+
         lab += 1
 
+    # Convert C++ vectors to numpy
+    cdef Py_ssize_t n_groups = splist.size()
+    cdef cnp.intp_t[:] splist_np = np.empty(n_groups, dtype=np.intp)
+    cdef cnp.intp_t[:] group_sizes_np = np.empty(n_groups, dtype=np.intp)
 
-    cdef long[:] splist_np = np.zeros(splist.size(), dtype=int)
-    cdef long[:] group_sizes_np = np.zeros(group_sizes.size(), dtype=int)
-    
-    for i in range(splist.size()):
+    for i in range(n_groups):
         splist_np[i] = splist[i]
         group_sizes_np[i] = group_sizes[i]
 
